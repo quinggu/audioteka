0b9e97586e523289d95caed347c610aeb49f8e04
Tomasz Białczak <tomasz.bialczak@gmail.com>
2024-06-13 08:25:52 +0200

    Implement transaction handling in CartRepository

--- src/Repository/CartRepository.php
+++ src/Repository/CartRepository.php
@@ -6,7 +6,9 @@ use App\Entity\CartProducts;
 use App\Entity\Product;
 use App\Service\Cart\Cart;
 use App\Service\Cart\CartService;
+use Doctrine\DBAL\LockMode;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Ramsey\Uuid\Uuid;
 
 class CartRepository implements CartService
@@ -15,19 +17,27 @@ class CartRepository implements CartService
 
     public function addProduct(string $cartId, string $productId): void
     {
-        $cart = $this->entityManager->find(\App\Entity\Cart::class, $cartId);
-        $product = $this->entityManager->find(Product::class, $productId);
+        $this->entityManager->beginTransaction();
+        try {
+            $cart = $this->entityManager->find(\App\Entity\Cart::class, $cartId, LockMode::PESSIMISTIC_WRITE);
+            $product = $this->entityManager->find(Product::class, $productId);
 
-        if ($cart && $product) {
-            if ($cart->hasProduct($product)) {
-                $cartProduct = $this->entityManager->getRepository(CartProducts::class)->findOneBy(['product' => $product]);
-                $cartProduct->setProductCounter($cartProduct->getProductCounter() + 1);
-            } else {
-                $cart->addProduct($product);
+            if ($cart && $product) {
+                if ($cart->hasProduct($product)) {
+                    $cartProduct = $this->entityManager->getRepository(CartProducts::class)->findOneBy(['product' => $product]);
+                    $cartProduct->setProductCounter($cartProduct->getProductCounter() + 1);
+                } else {
+                    $cart->addProduct($product);
+                }
+
+                $this->entityManager->persist($cart);
+                $this->entityManager->flush();
             }
 
-            $this->entityManager->persist($cart);
-            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
         }
     }
 
